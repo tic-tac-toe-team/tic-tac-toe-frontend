f85246d6a770c695ed086160540e604ef63b78bb
annamakiichuk <annmakiichuk@gmail.com>
2024-09-19 12:51:16 +0300

    refactor: rename folders and delete Game.tsx, rewrite components to arrow functions

--- src/components/LeaveGameButton/LeaveGameButton.tsx
+++ src/components/LeaveGameButton/LeaveGameButton.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './LeaveGameButton.css'
 
-export default function LeaveGameButton() {
+const LeaveGameButton = () => {
     return <button className="leave-game-button">Leave the game</button>;
 }
+
+export default LeaveGameButton;
