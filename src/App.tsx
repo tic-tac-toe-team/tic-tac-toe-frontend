3d91b486dd1f1b1eade53d0c0bbdb08d06808818
annamakiichuk <annmakiichuk@gmail.com>
2024-09-20 14:41:11 +0300

    refactor: remove unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import './App.css';
-import GamePage from "./components/GamePage/GamePage";
-import RoomList from "./components/RoomsList/RoomsList";
 
 function App() {
   return (
     <>
-      <GamePage></GamePage>
-      {/*  <RoomList></RoomList>*/}
     </>
   );
 }
